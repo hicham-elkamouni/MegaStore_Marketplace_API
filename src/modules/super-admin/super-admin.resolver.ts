82545174135f5b8d4696f593d88474da5e70a6a9
yassineelhoubi <elhoubiyassine@gmail.com>
2022-03-26 22:56:13 +0100

    add super admin logout functions

--- src/modules/super-admin/super-admin.resolver.ts
+++ src/modules/super-admin/super-admin.resolver.ts
@@ -1,9 +1,10 @@
 
 import { SuperAdmin } from './model/super-admin.model';
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Context } from '@nestjs/graphql';
 import { SuperAdminService } from './super-admin.service';
 import { Auth } from '../auth/model/auth.model';
 import { signinInput } from '../auth/dto/signin.input';
+import { Allowed } from '../auth/guards/Allowed.guard';
 @Resolver('SuperAdmin')
 export class SuperAdminResolver {
   constructor(private superAdminService: SuperAdminService) { }
@@ -16,6 +17,13 @@ export class SuperAdminResolver {
   @Mutation(() => Auth, { name: 'superAdminLogin' })
   async signIn(@Args('signinInput') signinInput: signinInput): Promise<Auth> {
     return this.superAdminService.signIn(signinInput)
+  }
+
+  @Allowed(['superAdmin'])
+  @Mutation(() => SuperAdmin, { name: 'superAdminLogout' })
+  async logOut(@Context('req') req: any): Promise<SuperAdmin> {
+    const userId = req.user?.userId;
+    return this.superAdminService.logOut(userId);
   }
 
 }
