53e9009d2fcaf22c3b8a1ffbb8d49a7618d1cc85
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-04-03 03:20:14 +0000

    implementing guards in shipping company just for admin

--- src/modules/shippingCompany/shippingCompany.resolver.ts
+++ src/modules/shippingCompany/shippingCompany.resolver.ts
@@ -3,31 +3,37 @@ import { CreateShippingCompanyInput } from './dto/createShippingCompany.input';
 import { Args, Mutation, Query , Resolver } from "@nestjs/graphql";
 import { ShippingCompany } from "./model/shippingCompany.model";
 import { ShippingCompanyService } from "./shippingCompany.service";
+import { Allowed } from '../auth/guards/Allowed.guard';
 
 @Resolver('ShippingCompany')
 export class ShippingCompanyResolver {
     constructor(private shippingCompanyService: ShippingCompanyService) {}
-
+    
+    @Allowed(['admin'])
     @Query(() => [ShippingCompany], { name : 'getAllShippingCompanies' })
     async getAll(): Promise<ShippingCompany[]> {
         return this.shippingCompanyService.getAll();
     }
 
+    @Allowed(['admin'])
     @Query(() => ShippingCompany, { name : 'getShippingCompanyById' })
     async getShippingCompanyById(@Args('id') id: string) {
         return this.shippingCompanyService.getShippingCompanyById(id);
     }
 
+    @Allowed(['admin'])
     @Mutation(() => ShippingCompany, { name : 'createShippingCompany' })
     async createShippingCompany(@Args('createShippingCompanyInput') createShippingCompanyInput: CreateShippingCompanyInput) {
         return this.shippingCompanyService.create(createShippingCompanyInput);
     }
 
+    @Allowed(['admin'])
     @Mutation(()=> ShippingCompany, { name : 'updateShippingCompany' })
     async updateShippingCompany(@Args('updateShippingCompanyInput') updateShippingCompanyInput: UpdateShippingCompanyInput) {
         return this.shippingCompanyService.updateShippingCompany(updateShippingCompanyInput);
     }
 
+    @Allowed(['admin'])
     @Mutation(()=> ShippingCompany, { name : 'deleteShippingCompany' })
     async deleteShippingCompany(@Args('id') id: string) {
         return this.shippingCompanyService.deleteShippingCompany(id);
